2f178c99a328a93dd0cd79f1ee7b24012fb3a570
Raihan Taher <raihan.taher@gmail.com>
2015-07-04 15:27:28 +0600

    showing day temp of a city and parameter for the number of days

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ weatherApp.config(function($routeProvider){
         templateUrl: 'pages/forcast.html',
         controller: 'forcastController'
     })
+    
+    .when('/forcast/:days', {
+        templateUrl: 'pages/forcast.html',
+        controller: 'forcastController'
+    })
 });
 
 // Service
@@ -32,6 +37,21 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
     });
 }]);
 
-weatherApp.controller('forcastController', ['$scope', 'cityService', function($scope, cityService){
+weatherApp.controller('forcastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
     $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || 2;
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback : "JSON_CALLBACK" }, { get: { method: "JSONP" } });
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q : $scope.city, cnt : $scope.days });
+    
+    $scope.convertToCelsius = function(degK){
+        return Math.round(degK - 273.15);
+    }
+    
+    $scope.convertToDate = function(dt){
+        return new Date(dt * 1000);   
+    }
+    
 }]);
